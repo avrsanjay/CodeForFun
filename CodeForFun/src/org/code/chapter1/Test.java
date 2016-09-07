dc6cca457a2c376e68b5ba179b1a428e59038760
206497230 <206497230@5TS206497230L7.tfayd.com>
2016-09-07 12:41:47 -0400

    Test Commmit

---
+++ CodeForFun/src/org/code/chapter1/Test.java
@@ -0,0 +1,9 @@
+package org.code.chapter1;
+class Test{
+	String temp = "Sanjay";
+	
+	public static void main(String args[]){
+		Test newobj = new Test();
+		System.out.println(newobj.temp);
+	}
+}
